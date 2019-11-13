510dd34c22f3c639a31720c3f6e157b4945bd2f8
nicovio <nicolasviotti90@gmail.com>
2019-11-13 19:10:38 -0300

    agrego componentes home, navbar y hotel-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HotelListComponent } from './components/hotel-list/hotel-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    HotelListComponent
   ],
   imports: [
     BrowserModule,
